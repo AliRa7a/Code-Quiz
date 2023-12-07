5a6e8d000982b5999cd6ac357a0179594f2e1c01
Ali Raza <alirazabinhussain@gmail.com>
2023-12-07 21:05:03 +0500

    Questions are added

---
+++ assets/js/questions.js
@@ -0,0 +1,30 @@
+const questions = [
+    {
+      question: "What does the acronym 'DOM' stand for in web development?",
+      choices: ["Document Object Model", "Data Object Model", "Digital Ordinance Model", "Dynamic Output Management"],
+      correctAnswer: "Document Object Model",
+    },
+    {
+      question: "What is the purpose of the 'localStorage' in JavaScript?",
+      choices: ["To store session-specific data", "To store data permanently on the client's machine", "To store data on the server", "To store temporary variables"],
+      correctAnswer: "To store data permanently on the client's machine",
+    },
+    {
+      question: "What is a closure in JavaScript?",
+      choices: ["A sealed block of code", "A function along with its lexical scope", "A conditional statement", "A JavaScript data type"],
+      correctAnswer: "A function along with its lexical scope",
+    },
+    {
+      question: "What is the difference between 'let' and 'const' in JavaScript?",
+      choices: ["'let' is used for constant values, and 'const' for variables", "'let' is block-scoped, and 'const' is function-scoped", "'let' allows reassignment, and 'const' does not", "'let' is used for integers, and 'const' for floating-point numbers"],
+      correctAnswer: "'let' allows reassignment, and 'const' does not",
+    },
+    {
+      question: "What does the 'this' keyword refer to in JavaScript?",
+      choices: ["The current function", "The global object", "The object the function is a property of", "The parameter of the current function"],
+      correctAnswer: "The object the function is a property of",
+    },
+  ];
+  
+  export default questions;
+  
